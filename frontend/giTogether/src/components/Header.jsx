143f0cd1c8e758969cfb1dc42ab0e30d4fef07b5
Adhish-Krishna <adhishthesak@gmail.com>
2024-10-30 19:59:29 +0530

    Added HomePage Component

---
+++ frontend/giTogether/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css'
+import PSGLogo from '../assets/PSG Logo.png';
+import GithubLogo from '../assets/GithubLogo.png';
+
+const Header = ()=>{
+  return(
+    <>
+    <div className="navbar">
+      <img src={PSGLogo} className="psglogo" />
+      <img src={GithubLogo} className="githublogo" />
+    </div>
+    </>
+  )
+}
+
+export default Header;
