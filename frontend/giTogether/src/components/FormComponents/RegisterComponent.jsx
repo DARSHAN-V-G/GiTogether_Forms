1c9158f493d343d07a547dd2c80ce8cd0637e39c
Adhish-Krishna <adhishthesak@gmail.com>
2024-11-02 11:22:41 +0530

    Changed font size for confirm text

--- frontend/giTogether/src/components/FormComponents/RegisterComponent.jsx
+++ frontend/giTogether/src/components/FormComponents/RegisterComponent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './FormComponents.css'
 const LoadingSpinner = () => {
     return <div className="loading-spinner"></div>;
   };
@@ -7,8 +7,8 @@ const LoadingSpinner = () => {
 const SummaryComponent = ({ formData, handleSubmit,startFromFirst, loading }) => {
   const text = `Gotcha ${formData.name}! As if we didn’t already know you’re a ${formData.department} genius, currently slaying it in your ${formData.year=='1' ? '1st' : '2nd'} year🎉! Check your inbox at ${formData.email} – or don’t.`
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', position: 'relative', padding:'10px' }}>
-      <h2>Confirm Your Details</h2>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100%', position: 'relative', padding:'10px',width:"80%"}}>
+      <h2 className="confirmtxt">Confirm Your Details</h2>
       <p style={{ margin: '1rem 0', color: 'white',textAlign:'center',fontSize:'18pt',fontFamily:"'Red Rose', serif"}}>{text}</p>
       <p style={{ margin: '1rem 0', color: 'white',textAlign:'center',fontSize:'18pt',fontFamily:"'Red Rose', serif" }}>If you need to change your details, Go back and Change</p>
       <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
